c06f44e3876af25b2ac8d17eb72bff57a97e98b9
mairo f <falcomzprime1@gmail.com>
2025-05-19 00:37:17 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// // Abrir modal
+// function openModal() {
+//     document.getElementById('modal').classList.remove('hidden');
+// }
+
+// // Cerrar modal
+// function closeModal() {
+//     document.getElementById('modal').classList.add('hidden');
+// }
+
+// // Cerrar al hacer clic fuera del modal
+// document.getElementById('modal').addEventListener('click', function(e) {
+//     if (e.target === this) {
+//         closeModal();
+//     }
+// });
+
+
